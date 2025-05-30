97fc85fc16b70ee8c8cd243457b0e3305833d46d
Lobsang S. <lobsangwhite@gmail.com>
2025-05-29 20:35:09 -0500

    chore: spawner added

--- Assets/TheGuitar/Scripts/Guitar/NoteSpawner.cs
+++ Assets/TheGuitar/Scripts/Guitar/NoteSpawner.cs
@@ -1,9 +1,44 @@
+using Sago.Guitar;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Sago
 {
     public class NoteSpawner : MonoBehaviour
     {
-        
+        [SerializeField]
+        private GameObject notePrefab;
+
+        [SerializeField]
+        private Transform spawnOrigin;
+
+        [SerializeField]
+        private int noteCount = 15;
+
+        private readonly Queue<GameObject> _notes = new();
+
+        private void Awake()
+        {
+            for (var i = 0; i < noteCount; i++)
+            {
+                var note = Instantiate(notePrefab, transform);
+
+                note.SetActive(false);
+
+                _notes.Enqueue(note);
+            }
+        }
+
+        public void Spawn()
+        {
+            if (_notes.Count <= 0)
+            {
+                return;
+            }
+
+            var note = _notes.Dequeue().GetComponent<StringNote>();
+
+            note.Init(transform.position, _notes.Enqueue);
+        }
     }
 }
---
+++ Assets/TheGuitar/Scripts/Guitar/StringNote.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Sago.Guitar
+{
+    public class StringNote : MonoBehaviour
+    {
+        private event Action<GameObject> Dispose;
+
+        private Vector2 _startPosition;
+
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _startPosition = transform.position;
+
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void Init(Vector2 origin, Action<GameObject> onDispose)
+        {
+            Invoke(nameof(OnDispose), 4);
+
+            transform.position = origin;
+
+            gameObject.SetActive(true);
+
+            Dispose = onDispose;
+
+            Animation();
+        }
+
+        public void Animation()
+        {
+            var randomDir = Random.insideUnitCircle.normalized;
+
+            float randomForce = Random.Range(500, 1000);
+
+            _rigidbody.AddForce(randomDir * randomForce);
+        }
+
+        private void OnDispose()
+        {
+            Dispose?.Invoke(gameObject);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
